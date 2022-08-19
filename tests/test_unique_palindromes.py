06206f6778622bc69390c8825a995b76c11a6b6a
HariKiran <harikiran.kante@gmail.com>
2022-08-19 22:30:19 +0530

    modified test cases

--- tests/test_unique_palindromes.py
+++ tests/test_unique_palindromes.py
@@ -2,8 +2,10 @@ from Python.unique_palindromes import get_unique_palindromes
 
 
 def test_get_unique_palindromes():
-    assert get_unique_palindromes("abcbada") == ['ada', 'abcba']
-    assert get_unique_palindromes("abcbadda") == ['adda', 'abcba']
+    assert get_unique_palindromes("abcbada") in [
+        ['ada', 'abcba'], ['abcba', 'ada']]
+    assert get_unique_palindromes("abcbadda") in [
+        ['adda', 'abcba'], ['abcba', 'adda']]
 
 
 def test_empty_string():
