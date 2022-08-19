bd10a7979c467de56df44d579099e86a7dc29888
HariKiran <harikiran.kante@gmail.com>
2022-08-19 22:40:44 +0530

    added comments

--- ADA/binary_search.py
+++ ADA/binary_search.py
@@ -1,12 +1,23 @@
+# Binary search using recursive approach
 def binary_search(arr: list, target, low=0, high=None) -> int:
     len_arr = len(arr)
+
+    # if high is not provided assign length of the array
     if high is None:
         high = len_arr
+
+    # calculate mid index
     mid = low+((high-low)//2)
+
+    # base condition
     if high < low:
         return -1
+
+    # return if found
     if target == arr[mid]:
         return mid
+
+    # recusive calls incase not found
     if target > arr[mid]:
         return binary_search(arr, target, mid+1, len_arr)
     else:
