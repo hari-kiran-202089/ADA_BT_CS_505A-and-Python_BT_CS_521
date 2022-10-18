b13f792f7c357b98d1d83515e181131a0417d2fb
HariKiran <harikiran.kante@gmail.com>
2022-10-19 00:15:50 +0530

    added Job class

--- Python/Job.py
+++ Python/Job.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 
+# Job class with 3 required properties
 @dataclass
 class Job:
     job_name: str
@@ -9,12 +10,15 @@ class Job:
 
 
 if __name__ == '__main__':
+    # taking input from users
     job_names = input("Enter Job names: ").split(" ")
     profits = list(map(int, input("Enter Profits: ").split(" ")))
     deadlines = list(map(int, input("Enter Deadlines: ").split(" ")))
 
+    # Storing all jobs in an list
     jobs = [Job(name, profit, deadline)
             for (name, profit, deadline) in zip(job_names, profits, deadlines)]
 
+    print("\nAll Jobs are:")
     for job in jobs:
         print(job)
