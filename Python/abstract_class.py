bac17ef671fb7432c8e79d6e2391690d599a7807
HariKiran <harikiran.kante@gmail.com>
2022-11-28 18:02:19 +0530

    added comments to the lab 9

--- Python/abstract_class.py
+++ Python/abstract_class.py
@@ -30,6 +30,7 @@ class BubbleSort(Sorting):
         for i in range(len(self.arr)):
             for j in range(len(self.arr)-i-1):
                 if self.arr[j] > self.arr[j+1]:
+                    # Swapping if left side value is greater
                     self.arr[j], self.arr[j+1] = self.arr[j+1], self.arr[j]
 
     def display(self):
@@ -52,6 +53,7 @@ class SelectionSort(Sorting):
             for j in range(i, len(self.arr)):
                 if self.arr[j] < self.arr[min_val_idx]:
                     min_val_idx = j
+            # swapping with the smallest value
             self.arr[i], self.arr[min_val_idx] = self.arr[min_val_idx], self.arr[i]
 
     def display(self):
@@ -61,8 +63,10 @@ class SelectionSort(Sorting):
 
 
 if __name__ == "__main__":
+    # taking user input
     array = [int(x) for x in input("Enter space seperated values: ").split()]
 
+    # instantiating Bubble Sort class
     print("\n-----Bubble Sort-----")
     b = BubbleSort(array)
     print("Before sorting: ")
@@ -71,6 +75,7 @@ if __name__ == "__main__":
     print("After sorting: ")
     b.display()
 
+    # instantiating Selection Sort class
     print("\n-----Selection Sort-----")
     s = SelectionSort(array)
     print("Before sorting: ")
